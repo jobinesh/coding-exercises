7badf34c3c098bd048b1a68795060227f70c14c9
Jobinesh Purushothaman <jobinesh.purushothaman@oracle.com>
2021-01-16 10:06:22 -0800

    initial commit

---
+++ src/main/java/com/jobinesh/exercise/App.java
@@ -0,0 +1,13 @@
+package com.jobinesh.exercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
